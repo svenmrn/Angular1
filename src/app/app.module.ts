af871e4daf687aca063b76ae4dd811e7903d0ddc
Sven <sven.marien01@ap.be>
2019-02-13 22:15:04 +0100

    page 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule
